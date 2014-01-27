701c491d5a6b7acc4ac6c1e0aae01e3673566fda
yijiang1121 <yijiang1121@gmail.com>
2014-01-26 16:32:46 -0800

    adding one more example to produce more colorful wordcloud

--- R/usingWordCloud.R
+++ R/usingWordCloud.R
@@ -1,6 +1,10 @@
 ### This R script shows how to use wordcloud R package
-library(wordcloud)
+library(wordcloud) # for using wordcloud
+library(RColorBrewer) # for using more colors
 
 ### state.abb and state.area are part of R "datasets" package
 wordcloud(words=state.name, freq=runif(n=length(state.name)) )
 wordcloud(words=state.abb, freq=state.area)
+
+### using more colors (instead of the default "black") and turn off random.order
+wordcloud(words=state.abb, freq=state.area, colors=brewer.pal(8,"Dark2"), random.order=F)
